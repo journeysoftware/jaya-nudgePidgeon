f3e935b8075ee606b1bb09dffebb9689cae662cb
Benze Gong <benzebgong@gmail.com>
2018-03-17 17:36:19 -0700

    convert to method and comment out sample payload

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import requests
 
+# examplePayload = {
+#    'issue': {
+#      'key': "BITE-1096"
+#    },
+#    'activity': 'gitCommit',
+#    'feedback': [
+#      {
+#        'text': "Excessive backlog/Overloaded queue/Burnout potential",
+#        'timeline': [
+#          ["2017-04-21T14:54:00.000Z", "2017-04-25T14:54:00.000Z"],
+#          ["2017-04-28T14:54:00.000Z"],
+#        ]
+#      }
+#    ]
+# }
+
 url = 'https://sleepy-beach-90505.herokuapp.com/nudges'
 
-headers = {
+def deliverNudge(payload):
+  global url
+  headers = {
     'Activity-Type': 'issuesDevstreamActivity'
-}
+  }
+  r = requests.post(url, json=payload, headers=headers)
+  print(r.status_code)
+  return
 
-# Example payload
-
-payload = {
-   'issue': {
-     'key': "BITE-1096"
-   },
-   'activity': 'gitCommit',
-   'feedback': [
-     {
-       'text': "Excessive backlog/Overloaded queue/Burnout potential",
-       'timeline': [
-         ["2017-04-21T14:54:00.000Z", "2017-04-25T14:54:00.000Z"],
-         ["2017-04-28T14:54:00.000Z"],
-       ]
-     }
-   ]
-}
-
-r = requests.post(url, json=payload, headers=headers)
-
-print(r.status_code)
+# deliverNudge(examplePayload)
